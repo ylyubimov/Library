dcf46846ed8b90f29c93a02af9bc9a227aebc848
anatastasia <anatastasia.f@gmail.com>
2015-09-24 14:40:23 +0300

    Контроллер BooksController переименован в RecordsController

--- Library/Controllers/RecordsController.cs
+++ Library/Controllers/RecordsController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace Library.Controllers
 {
-    public class BooksController : Controller
+    public class RecordsController : Controller
     {
         //
         // GET: /Books/
