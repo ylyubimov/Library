e485ae0945a1d9cd7c1c3d2fd55ecf4710ee8775
Dmitry Zvonarev <dmitry.zvonorev@phystech.edu>
2015-10-06 17:53:02 +0300

    using(db..) + вывод названия на изменения

--- Library/Controllers/AdminController.cs
+++ Library/Controllers/AdminController.cs
@@ -12,11 +12,10 @@ namespace Library.Controllers
 {
     public class AdminController : Controller
     {
-        private LibraryContext db = new LibraryContext();
 
         // GET: /Admin/
         public ActionResult Index()
-        {
+        { 
             return View();
         }
 
@@ -30,37 +29,46 @@ namespace Library.Controllers
         [HttpPost]
         public ActionResult Add(Record record)
         {
-            record.RecordId = db.Records.Count();
-            db.Records.Add(record);
-            db.SaveChanges();
+            using (LibraryContext db = new LibraryContext())
+            {
+                record.RecordId = db.Records.Count();
+                db.Records.Add(record);
+                db.SaveChanges();
 
-            return Redirect("/Admin/Index");
+                return Redirect("/Admin/Index");
+            }
         }
 
         [HttpGet]
         public ActionResult Edit(int id)
         {
-            bool idIsValid = (from r in db.Records
-                            select r.RecordId).Contains(id);
-            if (!idIsValid)
+            using (LibraryContext db = new LibraryContext())
             {
-                return new HttpNotFoundResult();
+                bool idIsValid = (from r in db.Records
+                                  select r.RecordId).Contains(id);
+                if (!idIsValid)
+                {
+                    return new HttpNotFoundResult();
+                }
+                var currentBook = (from r in db.Records where r.RecordId == id select r).First();
+                return View(currentBook);
             }
-
-            return View(new Record());
         }
 
         [HttpPost]
         public ActionResult Edit(int id, Record record)
         {
-            var query = (from r in db.Records
-                            where r.RecordId == id
-                            select r).First();
-            query.RecordName = record.RecordName;
-            query.RecordDescription = record.RecordDescription;
-            db.SaveChanges();
+            using (LibraryContext db = new LibraryContext())
+            {
+                var query = (from r in db.Records
+                             where r.RecordId == id
+                             select r).First();
+                query.RecordName = record.RecordName;
+                query.RecordDescription = record.RecordDescription;
+                db.SaveChanges();
 
-            return Redirect("/Admin/Index");
+                return Redirect("/Admin/Index");
+            }
 
         }
     }
