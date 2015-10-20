f3291f5bf3bf889ff17e0d3f71650959ebc57915
anatastasia <anatastasia.f@gmail.com>
2015-10-20 18:51:46 +0300

    Revert battle continues

---
+++ Library/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Library.Startup))]
+namespace Library
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
