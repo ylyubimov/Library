ca493a7c8831e086c6463e3398f673180700a2eb
ggoha <ryznikov@mail.ru>
2015-10-20 15:01:49 +0400

    merge+disable link

--- Library/Controllers/RecordsController.cs
+++ Library/Controllers/RecordsController.cs
@@ -35,63 +35,167 @@ namespace Library.Controllers
            return View(b);
         }
 
+        private List<SelectListItem> GetPublishersList()
+        {
+            List<SelectListItem> publishers = new List<SelectListItem>();
+            using (LibraryContext db = new LibraryContext())
+            {
+                var query = from p in db.Publishers orderby p.PublisherName select p;
+                foreach (var q in query)
+                {
+                    publishers.Add(new SelectListItem { Text = q.PublisherName, Value = (q.PublisherId + 1).ToString() });
+                    // для борьбы с 0-когда ничего не выбрали
+                }
+                return publishers;
+            }
+        }
+
+        private void EditRecord(Record record, string name, string description, Publisher publisher)
+        {
+            record.RecordName = name;
+            record.RecordDescription = description;
+            record.Author = publisher;
+        }
+
+        private ActionResult WriteToDataBaseRecordAndPublisher(int id, AdminAddEditModel model, bool add)
+        {
+            bool edit = !add;
+            using (LibraryContext db = new LibraryContext()) // Валидация книги
+            {
+                if ((db.Records.Where(rec => rec.RecordName == model.Record.RecordName).Count() > 0 && add)
+                    || (!(db.Records.Where(rec => (rec.RecordName == model.Record.RecordName) && (rec.RecordId == id)).Count() > 0
+                        || db.Records.Where(rec => (rec.RecordName == model.Record.RecordName)).Count() == 0)
+                        && edit))
+                {
+                    ModelState.AddModelError("Record.RecordName", "Книга с таким названием уже существует");
+                } // верная валидация = то же самое имя(его можно) || нет других имен из БД publishers
+            }
+            if (model.PublisherId != 0) // Воспользовались списком
+            {
+                if (ModelState.IsValidField("Record.RecordName") && ModelState.IsValidField("Record.RecordDiscription"))
+                {
+                    using (LibraryContext db = new LibraryContext())
+                    {
+                        int realPublisherId = model.PublisherId - 1;
+                        if (edit)
+                        {
+                            var recordQuery = (from r in db.Records
+                                               where r.RecordId == id
+                                               select r).First();
+                            Publisher publisher = (from p in db.Publishers where p.PublisherId == realPublisherId select p).First();
+                            EditRecord(recordQuery, model.Record.RecordName, model.Record.RecordDescription, publisher);
+                        }
+                        else
+                        {
+                            model.Record.Author = (from p in db.Publishers where p.PublisherId == realPublisherId select p).First();
+                            db.Records.Add(model.Record);
+                        }
+                        db.SaveChanges();
+                        return Redirect("/Records/Index");
+                    }
+                }
+                else
+                {
+                    model.Publishers = GetPublishersList();
+                    return View(model);
+                }
+            }
+
+            ModelState["PublisherId"].Errors.Clear(); // Игнорирования  [required] PublishersId в форме
+            using (LibraryContext db = new LibraryContext())  // Валидация создаваемого издательства
+            {
+                if (db.Publishers.Where(rec => rec.PublisherName == model.Record.Author.PublisherName).Count() > 0)
+                {
+                    ModelState.AddModelError("Record.Author.PublisherName", "Издатель с таким названием уже существует");
+                }
+            }
+            if (ModelState.IsValid) // С созданием нового publisher'a
+            {
+                using (LibraryContext db = new LibraryContext())
+                {
+                    if (edit)
+                    {
+                        var recordQuery = (from r in db.Records
+                                           where r.RecordId == id
+                                           select r).First();
+                        EditRecord(recordQuery, model.Record.RecordName, model.Record.RecordDescription, model.Record.Author);
+                    }
+                    else
+                    {
+                        db.Records.Add(model.Record);
+                    }
+                    db.Publishers.Add(model.Record.Author);
+                    db.SaveChanges();
+                    return Redirect("/Records/Index");
+                }
+            }
+            else
+            {
+                model.Publishers = GetPublishersList();
+                return View(model);
+            }
+        }
+
         [HttpGet]
         public ActionResult Add()
         {
-            return View(new Record());
+            AdminAddEditModel model = new AdminAddEditModel();
+            model.Publishers = GetPublishersList();
+            return View(model);
         }
 
 
         [HttpPost]
-        public ActionResult Add(Record record, IEnumerable<HttpPostedFileBase> fileUpload)
+        public ActionResult Add(AdminAddEditModel model, IEnumerable<HttpPostedFileBase> fileUpload)
         {
-            using (LibraryContext db = new LibraryContext())
-            {
-                record.RecordId = db.Records.Count();
-                record.PublisherId = db.Publishers.Count();
-                record.Author.PublisherId = record.PublisherId;
-                db.Publishers.Add(record.Author);
-                db.Records.Add(record);
-                db.SaveChanges();
-            }
             foreach (var file in fileUpload)
             {
                 if (file == null) continue;
                 string path = AppDomain.CurrentDomain.BaseDirectory + "Data/";
-                file.SaveAs(System.IO.Path.Combine(path, record.RecordName+".pdf"));
+                file.SaveAs(System.IO.Path.Combine(path, model.Record.RecordName + ".pdf"));
+            }            
+            return WriteToDataBaseRecordAndPublisher(0, model, true);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            int realId;
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(400, "Expected books id");
             }
-            return Redirect("/Records/Index");
+            else
+            {
+                realId = (int)id;
+            }
+            using (LibraryContext db = new LibraryContext())
+            {
+                bool idIsValid = (from r in db.Records
+                                  select r.RecordId).Contains(realId);
+                if (!idIsValid)
+                {
+                    return new HttpStatusCodeResult(404, "No book with such id: " + realId);
+                }
+                AdminAddEditModel model = new AdminAddEditModel();
+                model.Publishers = GetPublishersList();
+                model.Record = (from r in db.Records where r.RecordId == realId select r).First();
+                model.Record.Author = new Publisher();
+                model.PublisherId = model.Record.PublisherId + 1;
+                return View(model);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, AdminAddEditModel model)
+        {
+            return WriteToDataBaseRecordAndPublisher(id, model, false);
         }
 
         public FileResult downloadFile(int? id)
         {
             Record b = db.Records.Find(id);
-            return File("../../Data/"+b.RecordName+".pdf", "application/pdf");
-        }
-
-        [HttpGet]
-        public ActionResult Edit(int id)
-        {
-            using (LibraryContext db = new LibraryContext())
-            {
-                Record b = db.Records.Find(id);
-                return View(b);
-            }
-        }
-
-        [HttpPost]
-        public ActionResult Edit(int id, Record record)
-        {
-            using (LibraryContext db = new LibraryContext())
-            {
-                Record b = db.Records.Find(id);
-                b.RecordName = record.RecordName;
-                b.RecordDescription = record.RecordDescription;
-                db.SaveChanges();
-
-                return Redirect("/Record/Index");
-            }
-
+            return File("../../Data/" + b.RecordName + ".pdf", "application/pdf");
         }
 
     }
