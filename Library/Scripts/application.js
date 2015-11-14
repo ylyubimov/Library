ff96645553964730ecc906cb1f476305ceb0c093
ggoha <ryznikov@mail.ru>
2015-11-15 01:30:08 +0400

    Исправления для превью фтв ajax

---
+++ Library/Scripts/application.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $("#PublisherId").change(function(){
+        if ($("#PublisherId").val() == '')
+            $("#newPublisher").show();
+        else
+            $("#newPublisher").hide();
+    })
+})
