61d59cb37512764081df0f521a59dd3079876906
ggoha <ryznikov@mail.ru>
2015-11-24 18:00:39 +0400

    add name file upload

--- Library/Scripts/application.js
+++ Library/Scripts/application.js
@@ -1,8 +1,11 @@
 ﻿$(document).ready(function () {
-    $("#PublisherId").change(function(){
+    $("#PublisherId").change(function () {
         if ($("#PublisherId").val() == '')
             $("#newPublisher").show();
         else
             $("#newPublisher").hide();
-    })
+    });
+    $('input[type=file]').change(function () {
+        document.getElementById("test").innerHTML = this.value;
+    });
 })
